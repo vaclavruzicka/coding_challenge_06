48532cc1748042ce1243355486d9a41002d95828
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-02-07 22:47:15 -0500

    Changed line 21 from a mathFloor to a toFixed.

--- cc_6.js
+++ cc_6.js
@@ -18,7 +18,7 @@ let calculateSalesTax = function(amount, taxRate){
 
 //Logging the function and testing it with provided data.
 // // Had to use math floor to round down from 7.0001 to 7.
-console.log(`Sales Tax: $${Math.floor(calculateSalesTax(100, 0.07))}`) // Expected output: 7
+console.log(`Sales Tax: $${(calculateSalesTax(100, 0.07).toFixed(0))}`) // Expected output: 7
 console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`) // Expected output: 50
 
 //Task 3 - Arrow Function
